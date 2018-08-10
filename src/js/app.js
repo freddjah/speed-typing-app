fd0ec3044d366ee79be6fa071aee5b585592af22
unknown <fredrik.ekloof@gmail.com>
2018-08-11 00:50:01 +0200

    Adding points, resetting time on correct word.

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,13 @@ let timeLeftElement = document.querySelector('#timeLeft')
 let wordInputElement = document.querySelector('#wordInput')
 
 let timeLeft = 5.0
+let currentWord = 'Hello'
+let currentPoints = 0
+
+let setUp = () => {
+  updateCurrentWord('Hello')
+  setInterval(timeCountDown, 100)
+}
 
 let timeCountDown = () => {
   if (timeLeft > .1) {
@@ -16,4 +23,27 @@ let timeCountDown = () => {
   }
 }
 
-setInterval(timeCountDown, 100)
+let updateTimeLeft = (newTime) => {
+  timeLeft = newTime
+  timeLeftElement.innerText = Math.round(timeLeft * 10) / 10
+}
+
+let incrementCurrentPoints = () => {
+  currentPoints++
+  currentPointsElement.innerText = currentPoints
+}
+
+let updateCurrentWord = (newWord) => {
+  currentWord = newWord
+  currentWordElement.innerText = currentWord
+}
+
+wordInputElement.addEventListener('input', (_event) => {
+  if (currentWord === wordInputElement.value && timeLeft) {
+    console.log('Correct!')
+    updateTimeLeft(5.0)
+    incrementCurrentPoints()
+  }
+})
+
+setUp()
